fdf33923e418e1c3ccffdeb28df6facc0bb0c6b8
Facundo Perez <faacuu.peereezz@gmail.com>
2022-09-05 20:39:50 -0300

    entrega de proyecto

---
+++ JS/archivo.js
@@ -0,0 +1,159 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetchProductos()
+})
+
+const fetchProductos = async () => {
+    try {
+        const res = await fetch('productos.json')
+        const productos = await res.json()
+        mostrarArt(productos)
+        detectarBotones(productos)
+    } catch (error) {
+        console.log(error)
+    }
+}
+const articulos = document.getElementById('articulos')
+const templateArt = document.getElementById('templateArt').content
+const fragment = document.createDocumentFragment()
+const mostrarArt = (productos) =>{
+    //console.log(productos)
+    productos.forEach(producto =>{
+        templateArt.querySelector('h5').textContent = producto.nombre
+        templateArt.querySelector('p').textContent = producto.precio
+        templateArt.querySelector('img').setAttribute('src' , producto.imagen)
+        templateArt.querySelector('.btn-success').dataset.id = producto.id
+        const clone = templateArt.cloneNode(true)
+        fragment.appendChild(clone)   
+    })
+    articulos.appendChild(fragment)
+}
+let carrito = {}
+
+const detectarBotones = (data) => {
+    const botones = document.querySelectorAll('.card button')
+
+    botones.forEach(btn => {
+        btn.addEventListener('click', () => {
+            
+            const producto = data.find(item => item.id === parseInt(btn.dataset.id))
+            producto.cantidad = 1
+            if (carrito.hasOwnProperty(producto.id)) {
+                producto.cantidad = carrito[producto.id].cantidad + 1
+            }
+            carrito[producto.id] = { ...producto }
+            
+            pintarCarrito()
+        })
+    })
+}
+
+const items = document.getElementById('items')
+
+const pintarCarrito = () => {
+
+    const template = document.querySelector('#template-carrito').content
+    const fragment = document.createDocumentFragment()
+
+    Object.values(carrito).forEach(producto => {
+        
+        template.querySelector('th').textContent = producto.id
+        template.querySelectorAll('td')[0].textContent = producto.title
+        template.querySelectorAll('td')[1].textContent = producto.cantidad
+        template.querySelector('span').textContent = producto.precio * producto.cantidad
+        
+        //botones
+        template.querySelector('.btn-info').dataset.id = producto.id
+        template.querySelector('.btn-danger').dataset.id = producto.id
+
+        const clone = template.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+
+    items.appendChild(fragment)
+
+    mostrarFooter()
+    accionBotones()
+
+}
+
+const footer = document.querySelector('#footer-carrito')
+console.log(footer)
+const mostrarFooter = () => {
+
+    footer.innerHTML = ''
+console.log(footer)
+    if (Object.keys(carrito).length === 0) {
+        footer.innerHTML = `
+        <th scope='row' colspan='5'>Carrito vacío con innerHTML</th>
+        `
+        return
+    }
+
+    const template = document.querySelector('#template-footer').content
+    const fragment = document.createDocumentFragment()
+
+    // sumar cantidad y sumar totales
+    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
+   
+
+    template.querySelectorAll('td')[0].textContent = nCantidad
+    template.querySelector('span').textContent = nPrecio
+
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+
+    footer.appendChild(fragment)
+
+
+    const boton = document.querySelector('#vaciar-carrito')
+    boton.addEventListener('click', () => {
+        swal({
+            title: "Borrar carrito?",
+            text: "Si da click en OK se cancelara su pedido",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+              swal("Se vació su carrito con exito!", {
+                icon: "success",})
+              carrito = {}
+              pintarCarrito();
+            } else {
+              swal("Puede continuar con su compra!");
+            }
+          });
+        
+    })
+
+}
+
+const accionBotones = () => {
+    const botonesAgregar = document.getElementById('items .btn-info')
+    const botonesEliminar = document.getElementById('items .btn-danger')
+
+
+    botonesAgregar.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const producto = carrito[btn.dataset.id]
+            producto.cantidad ++
+            carrito[btn.dataset.id] = { ...producto }
+            pintarCarrito()
+        })
+    })
+
+    botonesEliminar.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const producto = carrito[btn.dataset.id]
+            producto.cantidad--
+            if (producto.cantidad === 0) {
+                delete carrito[btn.dataset.id]
+            } else {
+                carrito[btn.dataset.id] = { ...producto }
+            }
+            pintarCarrito()
+        })
+    })
+}
---
+++ JS/crud/crud.js
@@ -0,0 +1,154 @@
+
+let dbProd = localStorage.getItem("dbProd"); //Obtener datos de localStorage
+let operacion = "A"; //"A"=agregar; "E"=edtidar
+dbProd = JSON.parse(dbProd); // Convertir a objeto
+if (dbProd === null) // Si no existe, creamos un array vacio.
+    dbProd = [];
+
+
+function Mensaje(t){
+        switch (t) {
+            case 1: //
+                $(".mensaje-alerta").append(
+                    "<div class='alert alert-success' role='alert'>El producto se agrego con exito.</div>"
+                );
+                break;
+            case 2: //
+                $(".mensaje-alerta").append(
+                    "<div class='alert alert-danger' role='alert'>Se elimino el producto.</div>"
+                );
+                break;
+            default:
+
+        }
+    }
+
+
+function AgregarProd () {
+    // Seleccionamos los datos de los inputs de formulario
+    let datos_cliente = JSON.stringify({
+        Nombre : $("#nombre").val(),
+        id : $("#id").val(),
+        precio : $("#precio").val(),
+        urlImg : $("#urlImg").val(),
+    });
+
+    dbProd.push(datos_cliente); // Guardar datos en el array definido globalmente
+    localStorage.setItem("dbProd", JSON.stringify(dbProd));
+
+
+
+    ListarProductos();
+
+
+    return Mensaje(1);
+}
+
+
+
+function ListarProductos (){
+    $("#dbProd-list").html(
+            "<thead>" +
+                "<tr>" +
+                    "<th> ID </th>" +
+
+                    "<th> Nombre </th>" +
+                    "<th> id </th>" +
+                    "<th> precio </th>" +
+                    "<th> urlImg </th>" +
+                    "<th> </th>" +
+                    "<th>  </th>" +
+                "</tr>" +
+            "</thead>" +
+            "<tbody>" +
+            "</tbody>"
+    );
+
+    for (let i in dbProd) {
+        let d = JSON.parse(dbProd[i]);
+        $("#dbProd-list").append(
+                        "<tr>" +
+                            "<td>" + i + "</td>" +
+                            "<td>" + d.Nombre + "</td>" +
+                            "<td>" + d.id + "</td>" +
+                            "<td>" + d.precio + "</td>" +
+                            "<td>" + d.urlImg + "</td>" +
+                            "<td> <a id='"+ i +"' class='btnEditar' href='#'> <span class='glyphicon glyphicon-pencil'> </span>  </a> </td>" +
+                            "<td> <a id='" + i + "' class='btnEliminar' href='#'> <span class='glyphicon glyphicon-trash'> </span> </a> </td>" +
+                        "</tr>"
+                           );
+    }
+
+}
+
+
+if (dbProd.length !== 0) {
+    ListarProductos();
+} else {
+    $("#dbProd-list").append("<h2> No tienes productos. </h2>");
+}
+
+function contarproducts(){
+    let products = dbProd;
+    nProducts = products.length;
+
+    $("#numeroProd").append(
+        "<a>Actualmente hay" + "<br>" + "<span class='badge'>" + nProducts + "</span></a> productos."
+    );
+    return nProducts;
+}
+
+function Eliminar(e){
+    dbProd.slice(e, 1); // (posición en el array, numero de items a eliminar)
+    localStorage.setItem("dbProd", JSON.stringify(dbProd));
+    return Mensaje(2);
+}
+
+function Editar() {
+    dbProd[indice_selecionado] = JSON.stringify({
+        Nombre : $("#nombre").val(),
+        id : $("#id").val(),
+        precio : $("#precio").val(),
+        urlImg : $("#urlImg").val(),
+    });
+    localStorage.setItem("dbProd", JSON.stringify(dbProd));
+    operacion = "A"; //Regresamos  valor original
+    return true;
+
+}
+
+$(".btnEliminar").bind("click", function(){
+    alert(" Quieres eliminar el producto ?");
+    indice_selecionado = $(this).attr("id"); // "this" contiene el elemento clikeado en el contexto actual
+    console.log(indice_selecionado);
+    console.log(this);
+    Eliminar(indice_selecionado); // Eliminamos el elemento llamando la funcion de eliminar
+    ListarProductos();
+});
+
+$(".btnEditar").bind("click", function() {
+    alert("¿ Editar este producto ?");
+    $(".modo").html("<span class='glyphicon glyphicon-pencil'> </span> Modo edición");
+    operacion = "E";
+    indice_selecionado = $(this).attr("id");
+    console.log(indice_selecionado);
+    console.log(this);
+    
+    let prodItem = JSON.parse(dbProd[indice_selecionado]);
+    $("#nombre").val(prodItem.Nombre);
+    $("#id").val(prodItem.id);
+    $("#precio").val(prodItem.precio);
+    $("#urlImg").val(prodItem.urlImg);
+    $("#nombre").focus();
+});
+
+
+contarproducts();
+$("#products-form").bind("submit", function() {
+    debugger;
+    if (operacion == "A")
+        return AgregarProd();
+    else {
+        return Editar();
+    }
+});
